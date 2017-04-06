3a2476d02f714ef0c1d78b2d4fb349f8b02b3eef
1josh13 <1josh13@gmail.com>
2017-04-06 09:23:20 -0400

    Initial commit

---
+++ APFT_Tracker.py
@@ -0,0 +1,56 @@
+soldierNameList = []
+soldierAgeList = []
+looping = True
+
+class Soldier:
+    def __init__(self, firstname, lastname, age, gender):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.gender = gender
+    def get_name(self):
+        return self.firstname + " " + self.lastname
+    def set_name(self, firstname, lastname):
+        if self.firstname != firstname:
+            self.firstname = firstname
+        if self.lastname != lastname:
+            self.lastname = lastname
+    def get_age(self):
+        return self.age
+    def set_age(self, age):
+        self.age = age
+    def get_gender(self):
+        return self.gender
+    def set_gender(self, gender):
+        self.gender = gender
+
+while looping:
+    print (''' 
+    Main Menu
+    1) Select Soldier
+    2) View All Records
+    3) Add a Soldier
+    0) Exit Program
+    ''')
+    choice = input("Enter Selection> ")
+    if choice == "1":
+        if not soldierNameList:
+            print("There are no soldier records to display!")
+        else:
+            for i in soldierNameList:
+                print("Please make selection from list: ")
+                print(soldierNameList.index(i))
+    elif choice == "2":
+        print("Showing all records")
+    elif choice == "3":
+        firstname = input("Enter Soldier's first name: ")
+        surname = input("Enter Soldier's last name: ")
+        soldierage = input("Enter Soldier's Age: ")
+        soldiergender = input("Enter Soldier's Gender: ")
+        Soldier(firstname, surname, soldierage, soldiergender)
+        print("Solder, {} {}, age {} {} added to the database!".format(firstname, surname, soldierage, soldiergender))
+    elif choice == "0":
+        print("Goodbye!")
+        looping = False
+    else:
+        print("You've entered a incorrect option {}!".format(choice))
