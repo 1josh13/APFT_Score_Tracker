bcd63bc5dbb3d76c47f9b570386f63796f7c44aa
Joshua Rohr <1josh13@gmail.com>
2017-05-08 16:05:37 -0400

    Added female PU standards, added basic validation of gender and statements for proper male / female scores. Removed redundant code and func's.

--- APFT_Tracker.py
+++ APFT_Tracker.py
@@ -1,6 +1,7 @@
 import pickle
 from os.path import expanduser
 from metrics import pushup_groups_male
+from metrics import pushup_groups_female
 from metrics import situp_groups_unisex
 from metrics import run_groups_male
 homepath = expanduser("~\\Documents\\")
@@ -26,34 +27,41 @@ class Soldier:
             self.firstname = firstname
         if self.lastname != lastname:
             self.lastname = lastname
-#TODO: Remove lines 31-44 and check rest of code for issues.
-    def get_age(self):
-        return self.age
-    def set_age(self, age):
-        self.age = age
-    def get_gender(self):
-        return self.gender
-    def set_gender(self, gender):
-        self.gender = gender
-    def get_pushup_score(self):
-        return self.pushup_score
-    def get_situp_score(self):
-        return self.situp_score
-    def get_runtime_score(self):
-        return self.runtime_score
     def set_scores(self, pushup_reps, situp_reps, runtime, date):
-        self.pushup_score = pushup_groups_male[self.age_group][pushup_reps]
-        self.situp_score = situp_groups_unisex[self.age_group][situp_reps]
-        keylist = []
-        for key in run_groups_male[self.age_group].keys():
-            keylist.append(key)
-        while keylist.count(runtime) == 0:
-            runtime = runtime + 1
-        self.runtime_score = run_groups_male[self.age_group][runtime]
-        self.scorelist.append((date, self.pushup_score + self.situp_score + self.runtime_score))
+        if self.gender == "Male":
+            self.pushup_score = pushup_groups_male[self.age_group][pushup_reps]
+            self.situp_score = situp_groups_unisex[self.age_group][situp_reps]
+            keylist = []
+            for key in run_groups_male[self.age_group].keys():
+                keylist.append(key)
+            while keylist.count(runtime) == 0:
+                runtime = runtime + 1
+            self.runtime_score = run_groups_male[self.age_group][runtime]
+            total_score = self.pushup_score + self.situp_score + self.runtime_score
+            self.scorelist.append((date, pushup_reps, self.pushup_score, situp_reps, self.situp_score, runtime, self.runtime_score, total_score))
+        elif self.gender == "Female":
+            self.pushup_score = pushup_groups_female[self.age_group][pushup_reps]
+            self.situp_score = situp_groups_unisex[self.age_group][situp_reps]
+            keylist = []
+            for key in run_groups_male[self.age_group].keys():
+                keylist.append(key)
+            while keylist.count(runtime) == 0:
+                runtime = runtime + 1
+            #TODO: import female run standards, for now using male runtimes.
+            self.runtime_score = run_groups_male[self.age_group][runtime]
+            total_score = self.pushup_score + self.situp_score + self.runtime_score
+            self.scorelist.append((date, pushup_reps, self.pushup_score, situp_reps, self.situp_score, runtime,
+                                   self.runtime_score, total_score))
+        else:
+            print("Fatle Error, Can not set scores!")
     def print_scorelist(self):
         for entry in self.scorelist: # scorelist = [(str, int), (str, int), (str, int)]
-            print("Record Date: {} ------ Total Score: {}".format(entry[0], entry[1]))
+            print('''
+                Record Date: {}
+                PU Reps: {} -- PU Score: {}
+                SU Reps: {} -- SU Score: {}
+                Runtime: {} -- Run Score: {}
+                Total Score: {}'''.format(entry[0], entry[1], entry[2], entry[3], entry[4], entry[5], entry[6], entry[7]))
 
 def set_age_group(soldierage):
     if soldierage <= 21:
@@ -102,9 +110,8 @@ def print_main():
 def print_select():
     print('''
     Selection Menu
-    1) Select Soldier
-    2) Edit Selected Soldier Information
-    3) View / Add / Edit APFT Scores
+    1) Edit Selected Soldier Information
+    2) View / Add / Edit APFT Scores
     0) Return to Main Menu
     ''')
 
@@ -123,16 +130,6 @@ def print_edit_apft():
     3) Edit APFT Scores (Coming soon)
     0) Return to Selection Menu
     ''')
-
-def print_apft_list():
-    tmp.print_scorelist()
-    print('''
-    Pushups: {}
-    Situps: {}
-    2 Mile Run: {}
-
-    Total Score: {} out of 300
-    '''.format(tmp.get_pushup_score(), tmp.get_situp_score(), tmp.get_runtime_score(), tmp.get_pushup_score() + tmp.get_situp_score() + tmp.get_runtime_score()))
 
 def print_all_soldiers():
     print("Showing all soldier data")
@@ -171,8 +168,27 @@ def add_new_apft():
     2 Mile Run: {}
 
     Total Score: {} out of 300
-    '''.format(tmp.get_pushup_score(), tmp.get_situp_score(), tmp.get_runtime_score(),
-               tmp.get_pushup_score() + tmp.get_situp_score() + tmp.get_runtime_score()))
+    '''.format(tmp.pushup_score, tmp.situp_score, tmp.runtime_score,
+               tmp.pushup_score + tmp.situp_score + tmp.runtime_score))
+
+def edit_gender():
+    print("Select new gender:")
+    print("1. Male")
+    print("2. Female")
+    soldiergenderstr = int(input("> "))
+    if soldiergenderstr == 1:
+        soldiergender = "Male"
+    elif soldiergenderstr == 2:
+        soldiergender = "Female"
+    tmp.gender = soldiergender
+    print("The soldiers gender has been changed!")
+
+def edit_name():
+    new_firstname = input("Enter new first name: ")
+    new_lastname = input("Enter new last name: ")
+    tmp.firstname = new_firstname
+    tmp.lastname = new_lastname
+    print("The soldiers name has been changed!")
 
 soldier_list = loaddata(soldierdatafile)
 while main_loop:
@@ -181,49 +197,44 @@ while main_loop:
     if main_choice == "1":
         select_loop = True
         while select_loop:
+            #Prints soldier list and asks for user choice.
+            if not soldier_list:
+                print("There are no soldier records to display!")
+            else:
+                print("You MUST select a soldier to edit before continuing!!!")
+                for i in soldier_list:
+                    print(soldier_list.index(i), i.get_name(), i.gender, i.age)
+                soldier_select = int(input("Please enter your selection: "))
+                tmp = soldier_list[soldier_select]
             print_select()
             select_choice = input("Enter Selection: ")
-            #Prints soldier list and asks for user choice.
-            if select_choice == "1":
-                if not soldier_list:
-                    print("There are no soldier records to display!")
-                else:
-                    for i in soldier_list:
-                        print(soldier_list.index(i), i.get_name(), i.gender, i.age)
-                    soldier_select = int(input("Please enter your selection: "))
-                    tmp = soldier_list[soldier_select]
             #Enter "edit" mode to change selected soldiers attributes.
-            elif select_choice == "2":
+            if select_choice == "1":
                 edit_loop_main = True
                 while edit_loop_main:
                     print_edit()
                     edit_choice_info = input("Enter Selection: ")
                     if edit_choice_info == "1":
-                        new_firstname = input("Enter new first name: ")
-                        new_lastname = input("Enter new last name: ")
-                        tmp.set_name(new_firstname, new_lastname)
-                        print("The soldiers name has been changed!")
+                        edit_name()
                     elif edit_choice_info == "2":
                         new_age = int(input("Enter new age: "))
-                        tmp.set_age(new_age)
+                        tmp.age = new_age
                         print("The soldiers age has been changed!")
                     elif edit_choice_info == "3":
-                        new_gender = input("Enter the new gender: ")
-                        tmp.set_gender(new_gender)
-                        print("The soldiers gender has been changed!")
+                        edit_gender()
                     elif edit_choice_info == "0":
                         print("Returning to selection menu!")
                         edit_loop_main = False
                     else:
                         print("You've entered an incorrect option {}!".format(edit_choice_info))
             #TODO: Add ability to edit records.
-            elif select_choice == "3":
+            elif select_choice == "2":
                 edit_loop_apft = True
                 while edit_loop_apft:
                     print_edit_apft()
                     apft_loop_choice = input("Enter Selection: ")
                     if apft_loop_choice == "1":
-                        print_apft_list()
+                        tmp.print_scorelist()
                     elif apft_loop_choice == "2":
                         add_new_apft()
                     #TODO Edit apft scores.
